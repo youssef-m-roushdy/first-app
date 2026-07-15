b1c48f7013d764e5875f8804bc04aeb3201308a3
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2026-07-15 22:05:23 +0300

    docs: prcative data binding types

--- src/app/student/student.ts
+++ src/app/student/student.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-student',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './student.html',
   styleUrl: './student.css',
 })
 export class Student {
+  // interpolation binding using {{ name }}
   name = "Youssef";
+
+  changeName = () => {
+    this.name = "Youssef Elhaj";
+  }
+
+  // property binding using [src] attribute
+  imageUrl = "https://www.w3schools.com/howto/img_avatar.png";
+
+  isDisabled = true;
+
+
+  // event binding using (click) event
+  counter = 0;
+
+  increase = () => {
+    this.counter++;
+  }
+
+  // two way data binding using ngModel
+  nickname = "";
+
+
+
 }
